2153b786cbf83c327a70bf94ea106f29fa26bbf6
Samuel Zuniga Vanoye <samuelzv@gmail.com>
2017-01-16 00:04:34 -0600

    Add initial routing.

--- src/client/app/app.component.ts
+++ src/client/app/app.component.ts
@@ -2,6 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'tateti-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `<h1>Hello {{name}}</h1>
+  <a routerLink="/registration">Registration</a>
+  <a routerLink="/welcome">Welcome</a>
+  <router-outlet></router-outlet>
+  `,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  {
+  name = 'Angular';
+  constructor() {}
+}
--- src/client/app/app.module.ts
+++ src/client/app/app.module.ts
@@ -1,11 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule }      from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent }  from './app.component';
+import { RegistrationComponent } from './registration/components/registration.component';
+import { WelcomeComponent } from './welcome/components/welcome.component';
+
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  imports:      [
+    BrowserModule ,
+    RouterModule.forRoot([
+      {
+        path: 'registration',
+        component: RegistrationComponent
+      },
+      {
+        path: 'welcome',
+        component: WelcomeComponent
+      }
+    ])
+  ],
+  declarations: [
+    AppComponent,
+    RegistrationComponent,
+    WelcomeComponent
+  ],
   bootstrap:    [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {
+  constructor() {}
+}
