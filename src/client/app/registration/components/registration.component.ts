5f93f2b1abda43acd30269fbb1fd225359ad08d3
Samuel Zuniga Vanoye <samuelzv@gmail.com>
2017-01-16 00:04:20 -0600

    Add welcome and registration component.

---
+++ src/client/app/registration/components/registration.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'tate-registration',
+    moduleId: module.id,
+    templateUrl: 'registration.template.html',
+    styleUrls: ['registration.component.css']
+})
+export class RegistrationComponent  {
+    constructor() {}
+}
+
---
+++ src/client/app/welcome/components/welcome.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'tate-welcome',
+    moduleId: module.id,
+    templateUrl: 'welcome.template.html',
+    styleUrls: ['welcome.component.css']
+})
+export class WelcomeComponent  {
+    constructor() {}
+}
+
